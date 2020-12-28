1876ee1bb682399fc5aa4f894faa0edfb3ff2eca
Максим Сергеевич <krovorgen@gmail.com>
2020-12-28 16:33:09 +0300

    Complete phone input mask

--- src/js/script.js
+++ src/js/script.js
@@ -55,6 +55,8 @@ $(document).ready(function () {
     validateForms('#consultation-form');
     validateForms('#consultation form');
     validateForms('#order form');
+
+    $('input[name=phone]').mask("+375 (99) 999-99-99")
 });
 
 const slider = tns({
