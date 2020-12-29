2836727b48d13406e091fdf2b69e23c8a082f7e3
Максим Сергеевич <krovorgen@gmail.com>
2020-12-29 19:31:53 +0300

    complete pageup

--- src/js/script.js
+++ src/js/script.js
@@ -56,7 +56,36 @@ $(document).ready(function () {
     validateForms('#consultation form');
     validateForms('#order form');
 
-    $('input[name=phone]').mask("+375 (99) 999-99-99")
+    $('input[name=phone]').mask("+375 (99) 999-99-99");
+
+    $('form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 800) {
+            $(".pageup").fadeIn();
+        } else {
+            $(".pageup").fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+});
 });
 
 const slider = tns({
